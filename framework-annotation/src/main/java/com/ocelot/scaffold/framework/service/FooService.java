72c413acedebab832eaa8a83a503cb121c6bb2cd
rabbitcount <rabbitcount@gmail.com>
2015-09-30 17:15:50 +0800

    empty the branch

--- framework-annotation/src/main/java/com/ocelot/scaffold/framework/service/FooService.java
+++
@@ -1,9 +0,0 @@
-package com.ocelot.scaffold.framework.service;
-
-import com.ocelot.scaffold.framework.entity.User;
-
-public interface FooService {
-
-    public User doSomeBusinessStuff(long userId);
-
-}
--- framework-xml/src/main/java/com/ocelot/scaffold/framework/entity/status/IntEnum.java
+++
@@ -1,6 +0,0 @@
-package com.ocelot.scaffold.framework.entity.status;
-
-public interface IntEnum<E extends Enum<E>> {
-
-    int getIntValue();
-}
